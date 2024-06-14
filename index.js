ae6dc657c169a0c8b07953b6bcbfa611bef7fcf2
Harshwardhan-rawani <132639769+Harshwardhan-rawani@users.noreply.github.com>
2024-06-14 21:42:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const profile_router = require("./routers/profile.js")
 const userimage_router = require("./routers/userimage.js")
 const bodyParser = require('body-parser');
 const corsOptions = {
-    origin : "https://photogalleryfrontenc.vercel.app/"
+    origin : "https://photogalleryfrontenc.vercel.app"
     ,methods : "GET, POST, PUT, DELETE , PATCH , HEAD",
     credentials : true
 }
